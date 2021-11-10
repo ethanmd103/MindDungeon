1ccd05c10037972a1c1a055bb84e6d2a198401f4
Ethan Davis <ethanmd103@gmail.com>
2021-11-10 10:47:55 -0500

    first commit

---
+++ MindDungeon/ProgramUI.cs
@@ -0,0 +1,23 @@
+﻿using MindDungeon.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MindDungeon
+{
+    public class ProgramUI
+    {
+        public static void Run()
+        {
+            Player player = new Player();
+
+            Console.WriteLine("=====WELCOME TO THE MIND DUNGEON===");
+
+            Console.WriteLine("First we need your characters name, hit enter when you're done...:");
+            player.name = Console.ReadLine(); 
+
+        }
+    }
+}
